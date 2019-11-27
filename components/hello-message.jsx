72507363c303045c48bb54f4b203b6df468e6665
Maxime-Cao <maxime.cao20@gmail.com>
2019-11-27 11:24:51 +0100

    first commit

---
+++ components/hello-message.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class HelloMessage extends React.Component {
+    render() {
+        return (
+            <div className={
+                "container"
+            }>
+                <h1> {
+                    "Hello "} {this.props.name}
+                </h1> </div>
+        );
+    }
+}
+
+export default HelloMessage;
